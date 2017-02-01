1d33b11f7784b70cec7f67c81f213d7bc7a170d1
esell <esell@esheavyindustries.com>
2017-02-01 08:39:36 -0800

    not needed anymore

--- main.go
+++ main.go
@@ -155,13 +155,6 @@ func toInt(s string) int {
 	return i
 }
 
-func uuid() string {
-	b := make([]byte, 16)
-	Random.Read(b)
-	return fmt.Sprintf("%x-%x-%x-%x-%x",
-		b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
-}
-
 func httpErrorf(w http.ResponseWriter, format string, a ...interface{}) {
 	err := fmt.Errorf(format, a...)
 	log.Println(err)
